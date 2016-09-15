182ad23a871db3a7790bb096dbfc5f75aa06a16b
Wanderson S. Duarte <wandersonduarte.br@gmail.com>
2016-09-15 20:38:44 -0300

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,5 +1,6 @@
 from django.core import mail
 from django.test import  TestCase
+
 from eventex.subscriptions.forms import  SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
@@ -50,14 +51,16 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
+        """Valid POST should redirect to /inscricao/1/"""
         self.assertEqual(302, self.resp.status_code)
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
 
     def test_save_subscripion(self):
         self.assertTrue(Subscription.objects.exists())
+
 
 
 class SubscribePostInvalid(TestCase):
@@ -83,13 +86,3 @@ class SubscribePostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
-
-class SubscribeSuccessMessage(TestCase):
-
-    def test_message(self):
-        data = dict(nome='Wanderson Duate', cpf='12345678901',
-                    email='wsistemas.br@gmail.com', phone='21-999046793')
-
-        response = self.client.post('/inscricao/', data, follow=True)
-
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
