f5635e22f1a54a3a7e033568169a740d8d2faf33
Wanderson S. Duarte <wandersonduarte.br@gmail.com>
2016-09-19 13:37:35 -0300

    Adciona campo paid

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -4,10 +4,10 @@ from django.utils.timezone import now
 from eventex.subscriptions.models import Subscription
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today')
+    list_display = ('nome', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today', 'paid')
     date_hierarchy = 'created_at'
     search_fields = ('nome', 'email', 'phone', 'cpf', 'created_at')
-    list_filter = ('created_at', )
+    list_filter = ('created_at', 'paid')
 
     def subscribed_today(self, obj):
         return obj.created_at == now().date()
--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -25,3 +25,7 @@ class SubscriptionModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Wanderson Duarte', str(self.obj))
+
+    def test_paid_default_to_false(self):
+        """By default paid must be False."""
+        self.assertEqual(False, self.obj.paid)
