f208b5b9db181715d9858a19e19835527934523b
Wanderson S. Duarte <wandersonduarte.br@gmail.com>
2016-09-29 20:28:58 -0300

    Contatos

---
+++ eventex/core/tests/test_model_contact.py
@@ -0,0 +1,42 @@
+from unittest import skip
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from eventex.core.models import Speaker, Contact
+
+class ContactModelTeste(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            nome='Henrinque Bastos',
+            slug='henrique-bastos',
+            photo='http://hbn.link/hb-pic')
+
+    def test_email(self):
+        contact = Contact.objects.create(
+                    speaker = self.speaker,
+                    kind = Contact.EMAIL,
+                    value = 'henrique@bastos.net')
+
+        self.assertTrue(Contact.objects.exists())
+
+    def test_phone(self):
+        contact = Contact.objects.create(
+                    speaker = self.speaker,
+                    kind = Contact.PHONE,
+                    value = '21-996186180')
+
+        self.assertTrue(Contact.objects.exists())
+
+    @skip('Não está funcionando devidamente')
+    def test_choice(self):
+        """Contact kind should be limited to E or P"""
+        contact = Contact(speaker=self.speaker, kind='A', value='B')
+        self.assertRaises(ValidationError, contact.full_clean())
+
+    def test_str(self):
+        contact = Contact(
+            speaker=self.speaker,
+            kind=Contact.EMAIL,
+            value='henrique@bastos.net')
+
+        self.assertEqual('henrique@bastos.net', str(contact))
